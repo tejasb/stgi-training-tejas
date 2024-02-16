0d7ff384ae77a8e7106482e9b8226d6ba2145ad1
C Selvaraju <trainerselva@gmail.com>
2024-02-16 10:07:17 +0530

    First Commit

--- blog/views.py
+++ blog/views.py
@@ -8,6 +8,6 @@ def landing_page(request):
 def posts(request):
     return render(request, "blog/all-posts.html")
 
-def single_post(request):
-    pass
+def single_post(request, slug):
+    return render(request, "blog/post-detail.html")
 
